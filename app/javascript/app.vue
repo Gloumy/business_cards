023966e1aad32bcefe17ec56fc67cbfe21aef907
Simon <simon@cornet.dev>
2020-04-10 13:43:21 +0200

    ugly try at a preview

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,38 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div class="app">
+    <label for="firstname">Prénom</label>
+    <input type="text" v-model="firstname" name="firstname" id="firstname" />
+    <label for="lastname">Nom</label>
+    <input type="text" v-model="lastname" name="lastname" id="lastname" />
+    <div class="preview-container" :style="{'background-color': backgroundColor}">
+      <p>{{firstname}}</p>
+      <p>{{lastname}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
-      message: "Hello Vue!"
-    }
+      message: "Hello Vue!",
+      firstname: "",
+      lastname: "",
+      backgroundColor: "blue",
+    };
   }
-}
+};
 </script>
 
 <style scoped>
 p {
   font-size: 2em;
   text-align: center;
+}
+.preview-container {
+  border-style: solid;
+  border-color: black;
+  width: 200px;
+  height: 150px;
 }
 </style>
